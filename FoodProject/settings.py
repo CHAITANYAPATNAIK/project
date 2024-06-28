4cf7f0546eea0739f8ee805b34adf457c5fd6070
CHAITANYA PATNAIK <105854971+CHAITANYAPATNAIK@users.noreply.github.com>
2024-06-29 01:09:21 +0530

    Update settings.py

--- FoodProject/settings.py
+++ FoodProject/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['65.2.121.196','local hosts','0.0.0.0']
 
 
 # Application definition
